8cf1b5f2b74832feec3984d520b0e4bf6e16af81
tusonggao <tusonggao@163.com>
2019-05-16 12:17:17 +0800

    update files

--- keras_cnn_cifar_10.py
+++ keras_cnn_cifar_10.py
@@ -1,18 +1,16 @@
 # https://blog.csdn.net/yph001/article/details/82950570
-
-import time
-from keras.datasets import cifar10
 import numpy as np
+import time
+
+from keras.datasets import cifar10
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import Flatten
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.optimizers import SGD
 from keras.constraints import maxnorm
 from keras.utils import np_utils
 from keras import backend
+
 backend.set_image_data_format('channels_first')
 
 def batcher(X_data, y_data, batch_size=-1, random_seed=None):
@@ -43,8 +41,6 @@ x_train = x_train.astype('float32')
 x_validation = x_validation.astype('float32')
 x_train = x_train/255.0
 x_validation = x_validation/255.0
-
-# 进行one-hot编码
 y_train = np_utils.to_categorical(y_train)
 y_validation = np_utils.to_categorical(y_validation)
 num_classes = y_train.shape[1]
@@ -68,30 +64,40 @@ def create_model(epochs=25):
     model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
     return model
 
-epochs = 25
-model = create_model(epochs)
-
-print('start training!')
 
 # 训练模型及评估模型
 # model.fit(x=x_train, y=y_train, epochs=epochs, batch_size=32, verbose=2)
 # score = model.evaluate(x=x_validation, y=y_validation, verbose=0)
 # print('Accuracy: %.2f%%' % (score[1] * 100))
 
-total_iter_num = 0
-max_epochs = 5
-batch_size = 32
+print('x_train.shape is ', x_train.shape, 'y_train.shape is ', y_train.shape,
+      'x_validation.shape is ', x_validation.shape, 'y_validation.shape is ', y_validation.shape)
 
-for epoch in range(max_epochs):
-    print('current epoch is ', epoch)
-    for batch_X, batch_y in batcher(x_train, y_train, batch_size):
-        total_iter_num += 1
-        model.train_on_batch(batch_X, batch_y)
-        if total_iter_num%50==0:
-            train_score = model.evaluate(x=batch_X, y=batch_y, verbose=0)
-            val_score = model.evaluate(x=x_validation, y=y_validation, verbose=0)
-            print('epoch: ', epoch, 'total_iter_num: ', total_iter_num,
-                  'train Loss: {}, Accuracy: {} '.format(train_score[0], train_score[1]),
-                  'val Loss: {}, Accuracy: {}'.format(val_score[0], val_score[1]))
+def keras_cnn_test(x_data, y_data):
+    epochs = 25
+    model = create_model(epochs)
+
+    total_iter_num = 0
+    max_epochs = 10
+    batch_size = 32
+    for epoch in range(max_epochs):
+        print('current epoch is ', epoch)
+        for batch_X, batch_y in batcher(x_train, y_train, batch_size):
+            total_iter_num += 1
+            model.train_on_batch(batch_X, batch_y)
+            if total_iter_num%50==0:
+                train_score = model.evaluate(x=batch_X, y=batch_y, verbose=0)
+                val_score = model.evaluate(x=x_validation, y=y_validation, verbose=0)
+                print('epoch:{:3d} total_iter_num:{:6d}, Train Loss:{:.13f} Accuracy:{:.7f}, Val Loss:{:.13f} Accuracy: {:.7f}'.format(
+                    epoch, total_iter_num, train_score[0], train_score[1], val_score[0], val_score[1])
+                )
+
+
+if __name__=='__main__':
+    print('start training!')
+    keras_cnn_test(x_train, y_train)
+
+
+
 
 
