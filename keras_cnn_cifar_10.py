a21cb10fab4799eecca1c7e6c0a251d4ba846c1d
tusonggao <tusonggao@163.com>
2019-05-16 17:21:01 +0800

    update files

--- keras_cnn_cifar_10.py
+++ keras_cnn_cifar_10.py
@@ -3,13 +3,108 @@ import numpy as np
 import time
 
 from keras.datasets import cifar10
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, BatchNormalization
-from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Input, Add, Activation, Dropout
+from keras.layers.convolutional import Conv2D, Convolution2D, MaxPooling2D, AveragePooling2D, ZeroPadding2D
 from keras.optimizers import SGD
 from keras.constraints import maxnorm
-from keras.utils import np_utils
+from keras.utils import np_utils, plot_model
 from keras import backend
+from keras import backend as K
+from keras.regularizers import l2
+
+
+weight_decay = 0.0005
+
+
+def expand_conv(init, base, k, stride):
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+
+    shortcut = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(init)
+    shortcut = Activation('relu')(shortcut)
+
+    x = ZeroPadding2D((1, 1))(shortcut)
+    x = Convolution2D(base * k, (3, 3), strides=stride, padding='valid', kernel_initializer='he_normal',
+                      use_bias=False)(x)
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = Activation('relu')(x)
+
+    x = ZeroPadding2D((1, 1))(x)
+    x = Convolution2D(base * k, (3, 3), strides=(1, 1), padding='valid', kernel_initializer='he_normal',
+                      use_bias=False)(x)
+
+    shortcut = Convolution2D(base * k, (1, 1), strides=stride, padding='same', kernel_initializer='he_normal',
+                             use_bias=False)(shortcut)
+
+    m = Add()([x, shortcut])
+
+    return m
+
+
+def conv_block(input, n, stride, k=1, dropout=0.0):
+    init = input
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(input)
+    x = Activation('relu')(x)
+    x = Convolution2D(n * k, (3, 3), strides=(1, 1), padding='same', kernel_initializer='he_normal', use_bias=False)(x)
+
+    if dropout > 0.0: x = Dropout(dropout)(x)
+
+    x = BatchNormalization(axis=channel_axis, momentum=0.1, epsilon=1e-5, gamma_initializer='uniform')(x)
+    x = Activation('relu')(x)
+    x = Convolution2D(n * k, (3, 3), strides=(1, 1), padding='same', kernel_initializer='he_normal', use_bias=False)(x)
+
+    m = Add()([init, x])
+    return m
+
+
+def create_wide_residual_network(input_dim, nb_classes=100, N=2, k=1, dropout=0.0, verbose=1):
+    """
+    Creates a Wide Residual Network with specified parameters
+    :param input: Input Keras object
+    :param nb_classes: Number of output classes
+    :param N: Depth of the network. Compute N = (n - 4) / 6.
+              Example : For a depth of 16, n = 16, N = (16 - 4) / 6 = 2
+              Example2: For a depth of 28, n = 28, N = (28 - 4) / 6 = 4
+              Example3: For a depth of 40, n = 40, N = (40 - 4) / 6 = 6
+    :param k: Width of the network.
+    :param dropout: Adds dropout if value is greater than 0.0
+    :param verbose: Debug info to describe created WRN
+    :return:
+    """
+    ip = Input(shape=input_dim)
+    x = ZeroPadding2D((1, 1))(ip)
+    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    x = Convolution2D(16, (3, 3), padding='same', kernel_initializer='he_normal', use_bias=False)(x)
+    nb_conv = 4
+    x = expand_conv(x, 16, k, stride=(1, 1))
+    for i in range(N - 1):
+        x = conv_block(x, n=16, stride=(1, 1), k=k, dropout=dropout)
+        nb_conv += 2
+
+    x = expand_conv(x, 32, k, stride=(2, 2))
+
+    for i in range(N - 1):
+        x = conv_block(x, n=32, stride=(2, 2), k=k, dropout=dropout)
+        nb_conv += 2
+
+    x = expand_conv(x, 64, k, stride=(2, 2))
+
+    for i in range(N - 1):
+        x = conv_block(x, n=64, stride=(2, 2), k=k, dropout=dropout)
+        nb_conv += 2
+
+    x = AveragePooling2D((8, 8))(x)
+    x = Flatten()(x)
+
+    x = Dense(nb_classes, activation='softmax')(x)
+
+    model = Model(ip, x)
+    if verbose: print("Wide Residual Network-%d-%d created." % (nb_conv, k))
+    return model
+
 
 backend.set_image_data_format('channels_first')
 
@@ -56,7 +151,7 @@ def batcher(X_data, y_data, batch_size=-1, random_seed=None):
         np.random.seed(random_seed)
 
     rnd_idx = np.random.permutation(len(X_data))
-    print('rnd_idx[:10] is', rnd_idx[:10])
+    # print('rnd_idx[:10] is', rnd_idx[:10])
     n_batches = len(X_data) // batch_size
     for batch_idx in np.array_split(rnd_idx, n_batches):
         X_batch, y_batch = X_data[batch_idx], y_data[batch_idx]
@@ -104,7 +199,8 @@ def create_big_model(epochs=25):
     lrate = 0.01
     decay = lrate/epochs
     sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    # model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
 
@@ -142,6 +238,7 @@ def keras_cnn_test(x_data, y_data):
     batch_size = 32
     for epoch in range(max_epochs):
         print('current epoch is ', epoch)
+        epoch_start_t = time.time()
         for batch_X, batch_y in batcher(x_train, y_train, batch_size):
             total_iter_num += 1
             model.train_on_batch(batch_X, batch_y)
@@ -151,11 +248,20 @@ def keras_cnn_test(x_data, y_data):
                 print('epoch:{:3d} total_iter_num:{:6d}, Train Loss:{:.13f} Accuracy:{:.7f}, Val Loss:{:.13f} Accuracy: {:.7f}'.format(
                     epoch, total_iter_num, train_score[0], train_score[1], val_score[0], val_score[1])
                 )
+        print('epoch: ', epoch, 'cost time', time.time()-epoch_start_t)
 
 
 if __name__=='__main__':
     print('start training!')
-    keras_cnn_test(x_train, y_train)
+    # keras_cnn_test(x_train, y_train)
+
+    init = (3, 32, 32)
+
+    wrn_28_10 = create_wide_residual_network(init, nb_classes=10, N=4, k=10, dropout=0.0)
+
+    wrn_28_10.summary()
+
+    plot_model(wrn_28_10, "./WRN-28-8.png", show_shapes=True)
 
 
 
